cbfba5b09ee76450a3a378258ed14f972133229e
C. Prakasa Putra <39235898+cpputra@users.noreply.github.com>
2018-05-13 17:11:58 +0700

    Update Config.php

--- Config.php
+++ Config.php
@@ -1,1 +1,10 @@
-p
+<?php
+ 
+$databaseHost = 'localhost';
+$databaseName = 'tugas';
+$databaseUsername = 'root';
+$databasePassword = '';
+ 
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+ 
+?>
