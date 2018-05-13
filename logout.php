d96555cbb84514a3c043f21d522aa7b5fdf4f906
1617051009MAdnanAnasR <37886792+1617051009MAdnanAnasR@users.noreply.github.com>
2018-05-13 16:39:04 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,1 +1,15 @@
-p
+<?php
+   session_start();
+   session_destroy();
+?>
+<link rel='stylesheet' href='style.css'>
+        <body>
+          <font color='black'><center><h2>Bye Bye!!</h2></font>
+          <h3>Silahkan Kunjungi Website Kami Kembali</h3><br>
+          <div class='kotak'><br><br>
+            <div align='center'><b>Anda Berhasil Logout!</b> <br><br>
+              <a href='login.php'><button class="tombol">Login Lagi</button></a>
+            </div>
+          </div>
+        </body>
+
